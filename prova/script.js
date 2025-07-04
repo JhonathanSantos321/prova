de684f03b42e553c6ac5ba79f8aecbdfd9395310
JhonathanSantos321 <jhonathan.demarch@escola.pr.gov.br>
2025-07-04 08:00:10 -0300

    .

--- prova/script.js
+++ prova/script.js
@@ -3,6 +3,7 @@
 let time1;
 let time2;
 let time3;
+let time4;
 
 let nome1;
 let nome2;
@@ -17,7 +18,7 @@ function cadastro(){
 
 
     time1 = (window.prompt("digite o nome do time: ")).toUpperCase();
-    mostraHTML.innerHTML += `<p> ${time1} e seus integrantes sao: </p> `
+    mostraHTML.innerHTML += `<p> O time <b> ${time1} </b> e seus integrantes sao: </p> `
     
 
     nome1 = (window.prompt("digite o nome do jogador 1: "));
@@ -29,29 +30,55 @@ function cadastro(){
     nome3 = (window.prompt("digite o nome do jogador 3: "));
    
 
-    var idade_1 = Number(window.prompt("digite sua 1 nota: "));
+    idade_1 = Number(window.prompt("digite sua 1 idade: "));
+    mostraHTML.innerHTML += `<p> ${nome1} tem a idade de ${idade_1}: </p> `
 
-   var idade_2 = Number(window.prompt("digite sua 2 nota: "));
+    idade_2 = Number(window.prompt("digite sua 2 idade: "));
+    mostraHTML.innerHTML += `<p> ${nome2} tem a idade de ${idade_2}: </p> `
  
-   var idade_3 = Number(window.prompt("digite sua 3 nota: "));
-     
+    idade_3 = Number(window.prompt("digite sua 3 idade: "));
+    mostraHTML.innerHTML += `<p> ${nome3} tem a idade de ${idade_3}: </p> `
+
+
+     media_idade = parseInt(idade_1 + idade_2 + idade_3)/3;
+     mostraHTML.innerHTML += `<p> a media  de idade é: ${media_idade} </p>`
+     if(media_idade >= 15){
+      alert("o time 1 pode jogar o interclasse")
+      }else {
+        alert("o time 1 nao podera jogar o interclasse")
+      }
+      mostraHTML.innerHTML += `<p> / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /</p> `
+
+
+      time1 = (window.prompt("digite o nome do time: ")).toUpperCase();
+    mostraHTML.innerHTML += `<p> O time <b> ${time1} </b> e seus integrantes sao: </p> `
     
 
-     media_idade = (idade_1 + idade_2 + idade_3)/3;
-     mostraHTML.innerHTML += `<p> a media e de: ${media_idade} </p>`
-     console.log(media_idade)
-
-     
-
-
+    nome1 = (window.prompt("digite o nome do jogador 1: "));
    
-var nota1 = Number(window.prompt("digite sua nota: "));
-var nota2 = Number(window.prompt("digite sua 2 nota: "));
-var nota3 = Number(window.prompt("digite sua 3 nota: "));
+
+    nome2 = (window.prompt("digite o nome do jogador 2: "));
+  
     
-    
+    nome3 = (window.prompt("digite o nome do jogador 3: "));
    
-mostraHTML.innerHTML += `<p> ${nome3} tem ${idade_3} anos  </p>`
-    
+
+    idade_1 = Number(window.prompt("digite sua 1 idade: "));
+    mostraHTML.innerHTML += `<p> ${nome1} tem a idade de ${idade_1}: </p> `
+
+    idade_2 = Number(window.prompt("digite sua 2 idade: "));
+    mostraHTML.innerHTML += `<p> ${nome2} tem a idade de ${idade_2}: </p> `
+ 
+    idade_3 = Number(window.prompt("digite sua 3 idade: "));
+    mostraHTML.innerHTML += `<p> ${nome3} tem a idade de ${idade_3}: </p> `
+
+
+     media_idade = parseInt(idade_1 + idade_2 + idade_3)/3;
+     mostraHTML.innerHTML += `<p> a media  de idade é: ${media_idade} </p>`
+     if(media_idade <= 15){
+      alert("o time 1 pode jogar o interclasse")
+      }else {
+        alert("o time 1 nao podera jogar o interclasse")
+      }
 
 }
